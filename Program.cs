2f810fdc7bb2756da7b140376a6a9329ed5b472b
Sasha Chokolov <alexchokolov2008@gmail.com>
2026-03-02 22:48:38 +0100

    Practice1: namespace and calculator

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using MathTools;
+
+class Program
+{
+    static void Main()
+    {
+        int number_a = 5;
+        int number_b = 7;
+
+        Calculator calculator = new Calculator(number_a, number_b);
+
+        calculator.Multiply(number_a, number_b);
+    }
+}
+
+namespace MathTools
+{
+    class Calculator
+    {
+        public int a;
+        public int b;
+
+        public Calculator(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public int Multiply(int a, int b)
+        {            
+            int c = b * a;           
+            Console.WriteLine($"The sum is: {c} ");
+            return c;
+        }
+    }
+}
+
+
+    
